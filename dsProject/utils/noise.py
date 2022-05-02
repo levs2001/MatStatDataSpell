365907e665a98aec6e0520fa0afeb5664f8d7ce0
levs2001 <levs.2001@gmail.com>
2022-05-02 23:05:42 +0300

    Done lab 5

--- dsProject/utils/noise.py
+++ dsProject/utils/noise.py
@@ -1,9 +1,8 @@
 import numpy as np
 
 
-def get_with_noise(x_mass, noise_func):
+def get_with_noise(x_mass, noise_func, epsilon=0.1, factor=1):
     y_mass = []
-    epsilon = 0.1
     for el in x_mass:
-        y_mass.append(el + epsilon * noise_func())
+        y_mass.append(factor * el + epsilon * noise_func())
     return np.array(y_mass)
